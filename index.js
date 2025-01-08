ba6d0a7b7c7a3744933bb71b6e8e49d60887a043
jeevanhd <jeevan.hd@kalvium.community>
2025-01-08 15:13:44 +0530

    done

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const { resolve } = require("path");
-
-const app = express();
-const port = process.env.PORT || 8080;
+require("dotenv").config();
 
 const connect = require("./database");
 const routes = require("./routs");
-app.use(express.json());
+
 app.use(express.static("static"));
-require("dotenv").config();
+app.use(express.json());
+
+const app = express();
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.sendFile(resolve(__dirname, "pages/index.html"));
